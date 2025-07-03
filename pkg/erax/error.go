211a1b9660625d4b3ba5705f79a386539b8246bd
DangeL <92173667+DangeL187@users.noreply.github.com>
2025-07-03 22:31:56 +0300

    Add package code

---
+++ pkg/erax/error.go
@@ -0,0 +1,95 @@
+package erax
+
+import (
+	"errors"
+)
+
+type Error interface {
+	error
+
+	Unwrap() error
+
+	Msg() string
+	Meta(key string) (interface{}, Error)
+	Metas() map[string]interface{}
+
+	WithMeta(key string, value interface{}) Error
+	WithMetas(metas map[string]interface{}) Error
+}
+
+type MetaProvider interface {
+	Meta(key string) (interface{}, Error)
+}
+
+type ErrorType struct {
+	err  error
+	meta map[string]interface{}
+	msg  string
+}
+
+func (e *ErrorType) Unwrap() error {
+	return e.err
+}
+
+func (e *ErrorType) Error() string {
+	return e.err.Error()
+}
+
+func (e *ErrorType) Msg() string {
+	return e.msg
+}
+
+func (e *ErrorType) Meta(key string) (interface{}, Error) {
+	if e.Metas() != nil && len(e.Metas()) > 0 {
+		if val, ok := e.meta[key]; ok {
+			return val, nil
+		}
+	}
+
+	var current Error
+	ok := errors.As(e.err, &current)
+	if !ok {
+		return nil, NewFromString("key not found in error chain", "")
+	}
+	return current.Meta(key)
+}
+
+func (e *ErrorType) Metas() map[string]interface{} {
+	return e.meta
+}
+
+func (e *ErrorType) WithMeta(key string, value interface{}) Error {
+	if e.meta == nil {
+		e.meta = make(map[string]interface{})
+	}
+
+	e.meta[key] = value
+
+	return e
+}
+
+func (e *ErrorType) WithMetas(metas map[string]interface{}) Error {
+	if e.meta == nil {
+		e.meta = make(map[string]interface{})
+	}
+
+	for k, v := range metas {
+		e.meta[k] = v
+	}
+
+	return e
+}
+
+func New(err error, msg string) Error {
+	return &ErrorType{
+		err: err,
+		msg: msg,
+	}
+}
+
+func NewFromString(err string, msg string) Error {
+	return &ErrorType{
+		err: errors.New(err),
+		msg: msg,
+	}
+}
---
+++ pkg/erax/trace.go
@@ -0,0 +1,100 @@
+package erax
+
+import (
+	"errors"
+	"fmt"
+	"github.com/charmbracelet/lipgloss"
+	"sort"
+	"strings"
+)
+
+func Trace(err Error) string {
+	if err == nil {
+		return ""
+	}
+
+	return formatErrorChain(err, true)
+}
+
+func SetErrorColor(color lipgloss.Color) {
+	errorColor = color
+	errorText = lipgloss.NewStyle().Foreground(errorColor)
+}
+
+func SetKeyColor(color lipgloss.Color) {
+	keyColor = color
+	keyText = lipgloss.NewStyle().Foreground(keyColor)
+}
+
+func SetNormalColor(color lipgloss.Color) {
+	normalColor = color
+	branch1 = lipgloss.NewStyle().Foreground(normalColor).Render(" ├─ ")
+	branch2 = lipgloss.NewStyle().Foreground(normalColor).Render(" │ ")
+	branch3 = lipgloss.NewStyle().Foreground(normalColor).Render(" ╰─ ")
+	message = lipgloss.NewStyle().Foreground(normalColor).Render(" ▼ [ERROR TRACE]\n")
+}
+
+func formatError(text string) string {
+	lines := strings.Split(text, "\n")
+	output := ""
+	for lineIdx, line := range lines {
+		if lineIdx != 0 {
+			output += branch2 + " "
+		}
+		output += errorText.Render(line)
+		if lineIdx < len(lines)-1 {
+			output += "\n"
+		}
+	}
+	return output
+}
+
+func formatErrorChain(err Error, isFirst bool) string {
+	var sb strings.Builder
+
+	prefix := branch1
+	if isFirst {
+		prefix = message + "\n" + branch1
+	}
+
+	sb.WriteString(prefix + formatError(err.Msg()) + "\n")
+
+	keys := make([]string, 0, len(err.Metas()))
+	for key := range err.Metas() {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for i, key := range keys {
+		value := err.Metas()[key]
+		connector := " " + branch1
+		if i == len(keys)-1 {
+			connector = " " + branch3
+		}
+		sb.WriteString(fmt.Sprintf("%s%s%s: %v\n", branch2, connector, keyText.Render(key), value))
+	}
+
+	if unwrapped := err.Unwrap(); unwrapped != nil {
+		var next Error
+		if errors.As(unwrapped, &next) {
+			sb.WriteString(formatErrorChain(next, false))
+		} else {
+			sb.WriteString(branch3 + formatError(unwrapped.Error()))
+		}
+	}
+
+	return sb.String()
+}
+
+var (
+	errorColor  lipgloss.Color = "#f38ba8"
+	keyColor    lipgloss.Color = "#cba6f7"
+	normalColor lipgloss.Color = "#585b70"
+
+	errorText = lipgloss.NewStyle().Foreground(errorColor)
+	keyText   = lipgloss.NewStyle().Foreground(keyColor)
+
+	branch1 = lipgloss.NewStyle().Foreground(normalColor).Render(" ├─ ")
+	branch2 = lipgloss.NewStyle().Foreground(normalColor).Render(" │ ")
+	branch3 = lipgloss.NewStyle().Foreground(normalColor).Render(" ╰─ ")
+	message = lipgloss.NewStyle().Foreground(normalColor).Render(" ▼ [ERROR TRACE]")
+)
